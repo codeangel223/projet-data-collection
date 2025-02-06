159a833fdfa187465eaf02b8c4e973a1700f1d01
moussasamina <moussa.malle@samina.tech>
2025-02-06 11:48:34 +0000

    clean screen after click

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.title("Moussa MALLE - Data Collection Project - Dakar Auto Project - DIT")
 
 
 if st.button("Ressources"):
+    st.session_state.dashboard_clicked = False
     st.switch_page("pages/ressources.py")
 
 
@@ -17,9 +18,11 @@ nb_page = int(st.sidebar.number_input(
     'Sélectionnez le nombre de page ?', step=1, min_value=1, max_value=5))
 
 if st.sidebar.button("Bs4/Selenium"):
+    st.session_state.dashboard_clicked = False
     show_bs4_scrapping_data(st, nb_page)
 
 if st.sidebar.button("Web scrapper"):
+    st.session_state.dashboard_clicked = False
     show_webscrapper_data(st)
 
 if "dashboard_clicked" not in st.session_state:
@@ -47,5 +50,6 @@ if st.session_state.dashboard_clicked:
     show_dashboard(st, cat_selected_key, cat_selected_label)
 
 if st.sidebar.button("Evaluer le projet"):
+    st.session_state.dashboard_clicked = False
     st.write('<iframe src="https://ee.kobotoolbox.org/i/xnuAwepx" width="800" height="600"></iframe>',
              unsafe_allow_html=True)
